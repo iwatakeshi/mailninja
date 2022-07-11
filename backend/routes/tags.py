e3a1a2ec00f17046f351199a8d1ad31f014d2712
Takeshi <iwatakeshi@gmail.com>
2022-07-10 19:45:29 -0600

    Update tags.py

--- backend/routes/tags.py
+++ backend/routes/tags.py
@@ -1,23 +1,24 @@
 from fastapi import APIRouter
 
 router = APIRouter()
+tags = ['tags']
 
-@router.get('/tags/{tag_id}', tags=['tags'])
+@router.get('/tags/{tag_id}', tags=tags)
 async def get_one():
   return {}
 
-@router.get('/tags', tags=['tags'])
+@router.get('/tags', tags=tags)
 async def get_many():
   return []
 
-@router.post('/tags', tags=['tags'])
+@router.post('/tags', tags=tags)
 async def create():
   return {}
 
-@router.delete('/tags/{tag_id}', tags=['tags'])
+@router.delete('/tags/{tag_id}', tags=tags)
 async def delete_one():
   return {}
 
-@router.delete('/tags', tags=['tags'])
+@router.delete('/tags', tags=tags)
 async def delete_many():
   return []
