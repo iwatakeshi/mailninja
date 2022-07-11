b9bb2cb9e2d07f95862629176113b81fa37cf931
Takeshi <iwatakeshi@gmail.com>
2022-07-10 20:22:33 -0600

    Create contacts route

---
+++ backend/routes/contacts.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+tags = ['Contacts']
+
+@router.get('/contacts/{contact_id}', tags=tags)
+async def get_one():
+  return {}
+
+@router.get('/contacts', tags=tags)
+async def get_many():
+  return []
+
+@router.post('/contacts', tags=tags)
+async def create():
+  return {}
+
+@router.delete('/contacts/{contact_id}', tags=tags)
+async def delete_one():
+  return {}
+
+@router.delete('/contacts', tags=tags)
+async def delete_many():
+  return []
